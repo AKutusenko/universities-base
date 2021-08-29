8275c9fcd9c99c1b8cc5005afe14584eae08ef1b
Artur Kutusenko <kutusenko@gmail.com>
2021-08-29 19:15:51 +0300

    1

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,21 +1,45 @@
 import React from 'react';
 import shortid from 'shortid';
+import axios from 'axios';
 import { Button } from '../Button/Button';
 import { Title } from '../Title/Title';
 import { Form } from '../Form/Form';
 import { Card } from '../Card/Card';
 
+const BASE_URL = 'http://localhost:4000';
+
 class Section extends React.Component {
   state = {
     showed: false,
     cities: [],
+    loading: false,
+    error: '',
   };
 
   componentDidMount() {
-    const parsedSities = JSON.parse(localStorage.getItem('cities'));
-    if (parsedSities && Array.isArray(parsedSities)) {
-      this.setState({ cities: parsedSities });
-    }
+    // const parsedSities = JSON.parse(localStorage.getItem('cities'));
+    // if (parsedSities && Array.isArray(parsedSities)) {
+    //   this.setState({ cities: parsedSities });
+    // }
+
+    this.setState({ loading: true });
+
+    axios
+      .get(`${BASE_URL}/cities`)
+      .then(response => {
+        if (response.status === 200) {
+          this.setState({ cities: response.data });
+        }
+        if (response.status === 404) {
+          throw new Error(response.message || 'cities = не существует');
+        }
+      })
+      .catch(error => {
+        this.setState({ error: error.message });
+      })
+      .then(() => {
+        this.setState({ loading: false });
+      });
   }
 
   componentDidUpdate() {
@@ -26,6 +50,14 @@ class Section extends React.Component {
     this.setState({
       cities: this.state.cities.filter(city => city.id !== id),
     });
+    axios
+      .delete(`${BASE_URL}/cities/${id}`)
+      .then(response => {
+        if (response.status === 200) {
+          console.log('DELETED!!!');
+        }
+      })
+      .catch(error => {});
   };
 
   // handleClearAll = () => {
@@ -39,11 +71,13 @@ class Section extends React.Component {
   };
 
   render() {
-    const { cities, showed } = this.state;
+    const { cities, showed, loading, error } = this.state;
 
     return (
       <div>
         {cities.length ? <Title title={'Города'} /> : null}
+        {loading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
         {cities.map(city => {
           return (
             <Card
